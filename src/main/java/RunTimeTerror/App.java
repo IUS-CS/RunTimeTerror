6d25b5b2949abbe654b1fee617b1ae5aef719ba3
wschottl <wschottl@iu.edu>
2019-02-04 19:06:13 -0500

    starting java file

---
+++ src/main/java/RunTimeTerror/App.java
@@ -0,0 +1,13 @@
+package RunTimeTerror;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
