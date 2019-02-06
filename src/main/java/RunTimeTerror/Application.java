69955ce84f1dbe69517bfa27a5b9a9fde187b04c
wschottl <wschottl@iu.edu>
2019-02-06 11:17:09 -0500

    created display

---
+++ src/main/java/RunTimeTerror/Application.java
@@ -0,0 +1,31 @@
+package RunTimeTerror;
+
+import RunTimeTerror.Viewer.Display;
+import javax.swing.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class Application
+{
+    public static void main( String[] args )
+    {
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                Display view = new Display();
+                JFrame frame = new JFrame();
+                frame.setSize(800, 600);
+                frame.setLocationRelativeTo(null);
+                frame.setResizable(false);
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.getContentPane().add(view);
+                frame.setVisible(true);
+                view.requestFocus();
+                view.start();
+            }
+        });
+    }
+}
