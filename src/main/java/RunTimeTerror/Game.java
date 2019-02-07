51ae552d19af94b04301e6a38a38536fe9cbdc64
wschottl <wschottl@iu.edu>
2019-02-07 18:55:41 -0500

    got black screen back with FPS counter in console

--- src/main/java/RunTimeTerror/Game.java
+++ src/main/java/RunTimeTerror/Game.java
@@ -1,6 +1,7 @@
 package RunTimeTerror;
 
 import java.awt.*;
+import java.awt.image.BufferStrategy;
 
 /**
  * Hello world!
@@ -10,18 +11,82 @@ public class Game extends Canvas implements Runnable
 {
     public static final long serialVersionUID = 4602267202262990821L;
 
+    public static final int Width = 800, Height = Width / 4 * 3;
+    private Thread thread;
+    private boolean running = false;
 
-    public void run(){
+    public Game(){
+        new Display(Width, Height, "Jumpman Plumber", this);
 
     }
 
+    public void run(){
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60.0;
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        long timer = System.currentTimeMillis();
+        int frames = 0;
+        while(running){
+            long now = System.nanoTime();
+            delta += (now-lastTime)/ ns;
+            lastTime = now;
+            while(delta >= 1){
+                tick();
+                delta--;
+            }
+            if(running)
+                render();
+            frames++;
+
+            if(System.currentTimeMillis() - timer > 1000){
+                timer += 1000;
+                System.out.println("FPS: " + frames);
+                frames = 0;
+            }
+        }
+        stop();
+
+    }
+
+    private void tick(){
+
+    }
+
+    private void render(){
+        BufferStrategy bs = this.getBufferStrategy();
+        if(bs == null){
+            this.createBufferStrategy(3);
+            return;
+        }
+
+        Graphics g = bs.getDrawGraphics();
+
+        g.setColor(Color.black);
+        g.fillRect(0, 0, Width, Height);
+
+        g.dispose();
+        bs.show();
+    }
+
     public synchronized void start(){
+        thread = new Thread(this);
+        thread.start();
+        running = true;
+    }
+    public synchronized void stop(){
+        try{
+            thread.join();
+            running = false;
+        } catch(Exception e){
+            e.printStackTrace();
+        }
 
     }
 
     public static void main( String[] args )
     {
-
+        new Game();
 
     }
 
