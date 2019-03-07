13467bb12b9a61d8ab6b3c1397f31487d638c1b4
wschottl <wschottl@iu.edu>
2019-03-07 17:32:19 -0500

    adding object interface file that was missing from commit

---
+++ src/main/java/RunTimeTerror/Entities/object.java
@@ -0,0 +1,25 @@
+package RunTimeTerror.Entities;
+
+public interface object {
+
+    void setX(int x);
+
+    void setY(int y);
+
+    int getX();
+
+    int getY();
+
+    void setId(ID id);
+
+    ID getId();
+
+    void setVelX(int velX);
+
+    int getVelX();
+
+    void setVelY(int velY);
+
+    int getVelY();
+
+}
