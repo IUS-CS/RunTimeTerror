c3f3a0d21ea086b9dc4c0b08a432d73915ec7f32
wschottl <wschottl@iu.edu>
2019-04-03 17:48:48 -0400

    Removed Collision test and added player test This is because testing Player will test Collision as well.

--- src/test/java/RunTimeTerror/Entities/PlayerTest.java
+++ src/test/java/RunTimeTerror/Entities/PlayerTest.java
@@ -1,25 +1,52 @@
 package RunTimeTerror.Entities;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.awt.*;
 
 import static org.junit.Assert.*;
 
 public class PlayerTest {
 
-    @Test
-    public void updateMovement() {
+    Player tester;
+    int x,left,right;
+
+    @Before
+    public void setUp() throws Exception {
+        tester = new Player(10,10,10,10,ID.Player) {
+            @Override
+            public void tick() {
+
+            }
+
+            @Override
+            public void render(Graphics g) {
+
+            }
+        };
     }
 
     @Test
-    public void updateHorizontal() {
+    public void UpdateHorizontal(){
     }
 
     @Test
     public void moveRight() {
+        tester.setVelX(0);
+        tester.moveRight();
+        int results = tester.getVelX();
+        System.out.println("moving right for object");
+        assertEquals(2, results);
     }
 
     @Test
     public void moveLeft() {
+        tester.setVelX(0);
+        tester.moveLeft();
+        int results = tester.getVelX();
+        System.out.println("moving right for object");
+        assertEquals(-2, results);
     }
 
     @Test
